7cdecde236e4449cb9266aa253cbfb3a48082576
karim tarek <karimtarekabdelaziz@gmail.com>
2021-01-24 17:02:06 +0200

    uploade favicon 24/1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,47 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'forsa',
+    title: 'Forsa Tanya',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        type: 'image/x-icon',
+        href: '/apple-touch-icon.png',
+        sizes: '180x180',
+      },
+      {
+        rel: 'manifest',
+        href: '/site.webmanifest',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
+        rel: 'mask-icon',
+        type: 'image/png',
+        href: '/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon-16x16.png',
+        sizes: '16x16',
+      },
+      {
+        rel: 'manifest',
+        type: 'image/png',
+        href: '/site.webmanifest',
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
